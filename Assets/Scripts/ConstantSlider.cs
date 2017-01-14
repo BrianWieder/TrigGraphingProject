b307e2d761a073de669df2796f5f084a801a6551
Jackbuddy9 <bdubbs0424@gmail.com>
2017-01-14 15:01:44 -0500

    Fixed gitignore, and added descriptions to each of the constants

---
+++ Assets/Scripts/ConstantSlider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ConstantSlider : MonoBehaviour {
+
+    [SerializeField]
+    private Text descriptionTextBox;
+
+    [SerializeField]
+    private string descriptionText;
+
+    private void Start()
+    {
+        EventTrigger trigger = GetComponentInParent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerEnter;
+        entry.callback.AddListener((eventData) => { OnMouseEnter(); });
+        trigger.triggers.Add(entry);
+
+        EventTrigger.Entry entry2 = new EventTrigger.Entry();
+        entry2.eventID = EventTriggerType.PointerExit;
+        entry2.callback.AddListener((eventData) => { OnMouseExit(); });
+        trigger.triggers.Add(entry2);
+    }
+
+    public void OnMouseEnter()
+    {
+        print("Enter");
+        descriptionTextBox.text = descriptionText;
+    }
+
+    public void OnMouseExit()
+    {
+        descriptionTextBox.text = "";
+    }
+
+
+}
